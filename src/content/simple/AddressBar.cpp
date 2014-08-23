065a0e631b3a3681e8a32ee87e333cec9f153405
plxaye <307685235@qq.com>
2014-08-23 21:46:07 +0800

    完善CAddressBar

--- src/content/simple/AddressBar.cpp
+++ src/content/simple/AddressBar.cpp
@@ -15,3 +15,49 @@ LRESULT CAddressBar::OnCloseCmd(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/
   EndDialog(wID);
   return 0;
 }
+
+LRESULT CAddressBar::OnBack(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+  MessageBox(L"OnBack");
+  return 0;
+}
+
+LRESULT CAddressBar::OnForward(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+  MessageBox(L"OnForward");
+  return 0;
+}
+
+LRESULT CAddressBar::OnReload(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+  MessageBox(L"OnReload");
+  return 0;
+}
+
+LRESULT CAddressBar::OnStop(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+  MessageBox(L"OnStop");
+  return 0;
+}
+
+void CAddressBar::SetUrl(CString url)
+{
+  CEdit edit;
+  edit.Attach(GetDlgItem(IDC_EDIT1));
+  edit.SetWindowText(url);
+}
+
+CString CAddressBar::GetUrl()
+{
+  CEdit edit;
+  CString url;
+  int nLength;
+  LPTSTR pszText;
+
+  edit.Attach(GetDlgItem(IDC_EDIT1));
+  nLength = edit.GetWindowTextLength();
+  pszText = url.GetBuffer(nLength+1);
+  edit.GetWindowText(pszText);
+  url.ReleaseBuffer(nLength);
+  return url;
+}
--- src/content/simple/AddressBar.h
+++ src/content/simple/AddressBar.h
@@ -9,6 +9,10 @@ public:
     MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
     COMMAND_ID_HANDLER(IDOK, OnCloseCmd)
     COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
+    COMMAND_ID_HANDLER(IDC_BACK, OnBack)
+    COMMAND_ID_HANDLER(IDC_FORWARD, OnForward)
+    COMMAND_ID_HANDLER(IDC_RELOAD, OnReload)
+    COMMAND_ID_HANDLER(IDC_STOP, OnStop)
   END_MSG_MAP()
 
   // Handler prototypes (uncomment arguments if needed):
@@ -18,4 +22,10 @@ public:
 
   LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
   LRESULT OnCloseCmd(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+  LRESULT OnBack(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+  LRESULT OnForward(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+  LRESULT OnReload(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+  LRESULT OnStop(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+  void SetUrl(CString url);
+  CString GetUrl();
 };
